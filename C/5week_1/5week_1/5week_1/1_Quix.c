823642b87f8a811335a3bdd8448e79b84757b90c
DooNas <jaejoon4929@gmail.com>
2022-05-12 18:48:04 +0900

    [ C ] 입출력 3가지 종류

---
+++ C/5week_1/5week_1/5week_1/1_Quix.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	char name[30];
+	int year;
+
+	printf("이름을 입력하세요 : ");
+	gets_s(name, 30);
+
+	printf("성별을 입력하세요 : ");
+	char sex = getchar();
+
+	printf("탄생년도를 입력하세요 : ");
+	scanf("%d", &year);
+
+	puts(name);
+	putchar(sex);
+	printf("\n");
+	printf("%d", year);
+
+
+	return 0;
+}
---
+++ C/5week_1/5week_1/5week_1/1_print.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main(void) {
+//	int i;
+//	/*char a;
+//	scanf("%c", &a);
+//	*/
+//	
+//	//char a = getchar();
+//	char a[99];
+//	gets_s(a, 99);
+//
+//
+//	//printf("%s", a);
+//	puts(a)
+//	return 0;
+//}
