3cbac5e171a915c096aa842ce18c14d055ae0744
DooNas <jaejoon4929@gmail.com>
2022-05-19 20:48:25 +0900

    오후수업 커밋

---
+++ C/6week_1/6week_@/6week_@/main.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+   
+    //for(int i = 0; i < 10; i++) printf("%d번째 안아줘요 \n", i);
+    //puts("------------------");
+    //int i = 0;
+    //while(i < 10){
+    //    printf("%d번째 안아줘요 \n", i);
+    //    i++;
+    //}
+    //i = 0;
+    //puts("------------------");
+    //do {
+    //    printf("%d번째 안아줘요 \n", i);
+    //    i++;
+    //} while (i < 10);
+    //puts("------------------");
+    
+    //가위 1 바위 2 보 3
+    //이길때까지 반복
+    //while(1){
+    //    int user;
+    //    int com;
+    //    com = rand() % 3 + 1; // (0 ~ 2) + 1 = (1 ~ 3)
+    //    printf("가위바위보를 입력하시요(가위 1, 바위 2, 보 3) : ");
+    //    scanf("%d", &user);
+    //    int sum = user - com;
+    //    if (sum == -2 || sum == 1) {
+    //        printf("사용자가 이겼습니다.\n");
+    //        break;
+    //    }
+    //    else if (sum == -1 || sum == 2) {
+    //        printf("컴퓨터가 이겼습니다.\n");
+    //    }
+    //    else {
+    //        printf("비겼습니다.\n");
+    //    }
+    //}
+
+    int num = 1;
+    while (num < 100) {
+
+    }
+
+    return 0;
+}
